c9855511a8e2ac7d6d1a07e74461ed66f1526de5
aixoio <79969530+aixoio@users.noreply.github.com>
2023-09-08 21:49:15 +1000

    Format code

--- src/parseboard.cpp
+++ src/parseboard.cpp
@@ -6,37 +6,37 @@
 
 minesweeperbot::board::Board* minesweeperbot::parseboard::parseboard(std::string data) {
 
-    std::istringstream iss(data);
-    std::string line;
-    std::vector<std::vector<unsigned char> > tileStates;
+  std::istringstream iss(data);
+  std::string line;
+  std::vector<std::vector<unsigned char> > tileStates;
 
-    while (std::getline(iss, line)) {
-        std::vector<unsigned char> row;
-        std::istringstream lineStream(line);
-        unsigned char tileState;
+  while (std::getline(iss, line)) {
+    std::vector<unsigned char> row;
+    std::istringstream lineStream(line);
+    unsigned char tileState;
 
-        while (lineStream >> tileState) {
-            row.push_back(tileState);
-        }
-
-        tileStates.push_back(row);
+    while (lineStream >> tileState) {
+      row.push_back(tileState);
     }
 
-    minesweeperbot::board::Board* board = new minesweeperbot::board::Board();
-    unsigned int numRows = tileStates.size();
-    unsigned int numCols = (numRows > 0) ? tileStates[0].size() : 0;
+    tileStates.push_back(row);
+  }
 
-    board->create(numRows, numCols);
+  minesweeperbot::board::Board* board = new minesweeperbot::board::Board();
+  unsigned int numRows = tileStates.size();
+  unsigned int numCols = (numRows > 0) ? tileStates[0].size() : 0;
 
-    for (unsigned int i = 0; i < numRows; ++i) {
-        for (unsigned int j = 0; j < numCols; ++j) {
-            unsigned char tileState = tileStates[i][j];
-            minesweeperbot::board::Tile* tile = new minesweeperbot::board::Tile(tileState);
-            board->setTile(tile, i, j);
-        }
+  board->create(numRows, numCols);
+
+  for (unsigned int i = 0; i < numRows; ++i) {
+    for (unsigned int j = 0; j < numCols; ++j) {
+      unsigned char tileState = tileStates[i][j];
+      minesweeperbot::board::Tile* tile = new minesweeperbot::board::Tile(tileState);
+      board->setTile(tile, i, j);
     }
+  }
 
-    return board;
+  return board;
 
 }
 
